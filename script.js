578e356ecf68e8c30f3911434ce7269395f8c49f
Lake Heckaman <lakeheckaman@gmail.com>
2022-12-01 18:38:51 -0500

    errrr

--- script.js
+++ script.js
@@ -546,7 +546,12 @@ const noiseShader = compileShader(gl.FRAGMENT_SHADER, `
     uniform vec3 uScale;
     uniform float uAspect;
     
-    
+    #define Index 1
+    #define PI 3.141592653589793
+    #define TWOPI 6.28318530718
+
+
+
     vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
     vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
     vec3 permute(vec3 x) { return mod289(((x*34.0)+1.0)*x); }
