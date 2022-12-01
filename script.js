31ad822cac6793750afb88d2d3fdc633e8e190ae
Lake Heckaman <lakeheckaman@gmail.com>
2022-12-01 07:49:37 -0500

    ttt

--- script.js
+++ script.js
@@ -222,7 +222,7 @@ function supportRenderTextureFormat (gl, internalFormat, format, type) {
 function startGUI () {
     //dat is a library developed by Googles Data Team for building JS interfaces. Needs to be included in project directory 
     var gui = new dat.GUI({ width: 300 });
-    gui.add(config, 'DYE_RESOLUTION', { 'high': 1024, 'medium': 512, 'low': 256, 'very low': 128 }).name('quality_8').onFinishChange(initFramebuffers);
+    gui.add(config, 'DYE_RESOLUTION', { 'high': 1024, 'medium': 512, 'low': 256, 'very low': 128 }).name('quality_9').onFinishChange(initFramebuffers);
     gui.add(config, 'SIM_RESOLUTION', { '32': 32, '64': 64, '128': 128, '256': 256 }).name('sim resolution').onFinishChange(initFramebuffers);
     gui.add(config, 'DENSITY_DISSIPATION', 0, 4.0).name('density diffusion');
     gui.add(config, 'VELOCITY_DISSIPATION', 0, 4.0).name('velocity diffusion');
@@ -1419,8 +1419,9 @@ function render (target) {
         drawColor(target, normalizeColor(config.BACK_COLOR));
         if (target == null && config.TRANSPARENT)
         drawCheckerboard(target);
-        // drawDisplay(target);
-        blit(picture);
+        drawDisplay(target);
+        // blit(picture);
+    
     }
     
     function drawColor (target, color) {
