370da525c44c6c080a6f4108dbf6c8eb854c5c04
Lake Heckaman <lakeheckaman@gmail.com>
2022-12-01 19:04:58 -0500

    errrrrrrrrrr

--- script.js
+++ script.js
@@ -551,7 +551,7 @@ const noiseShader = compileShader(gl.FRAGMENT_SHADER, `
     #define PI 3.141592653589793
     #define TWOPI 6.28318530718
 
-    varying vec2 vUv;
+    varying vec2 vUV;
 
 
     vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
